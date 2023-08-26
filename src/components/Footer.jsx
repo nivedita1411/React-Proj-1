657e737141bdfcdb7590827100d1ccbb2da33bfd
nivedita1411 <niveditayadav55@gmail.com>
2023-08-26 21:20:09 +0530

    React Proj

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Footer = () => {
+  return <footer>
+    <div>
+        <h1>
+            TechyStar
+        </h1>
+        <p>@all right reserved</p>
+    </div>
+    <div>
+        <h5>Follow Us!</h5>
+        <div>
+            <a href = "https://www.youtube.com/" target = {"blank"}>Youtube</a>
+            <a href = "https://www.instagram.com/" target = {"blank"}>Instagram</a>
+            <a href = "https://www.linkedin.com/in/nivedita-yadav14/" target = {"blank"}>LinkedIn</a>
+            <a href = "https://github.com/nivedita1411" target = {"blank"}>GitHub</a>
+        </div>
+    </div>
+  </footer>
+}
+
+export default Footer
